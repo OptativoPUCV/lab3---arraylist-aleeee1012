070265dcfd6b872761c3df81a39be15278ee8521
Replit user <>
2023-03-30 15:52:40 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -25,7 +25,7 @@ void append(ArrayList * l, void * data)
 {
   if(l->capacity <= l->size)
   {
-    l->size **= 2;
+    l->size = l->size * l->size;
     l->data = (void*)realloc(l->data, l->size * sizeof(void*));
     l->size++;
   }
